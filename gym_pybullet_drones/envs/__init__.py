0ec3d8bc23a895c91a00ff80853bd41af982809b
afien <109566236+afien@users.noreply.github.com>
2023-11-25 15:57:30 +0800

    Add files via upload

---
+++ gym_pybullet_drones/envs/__init__.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.insert(0, 'C:/Users/benson/Desktop/vscode/gym-pybullet-drones-main/gym-pybullet-drones-main')
+
+from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
+from gym_pybullet_drones.envs.VelocityAviary import VelocityAviary
---
+++ gym_pybullet_drones/examples/test.py
@@ -0,0 +1,7 @@
+# import sys
+# sys.path.insert(0, 'C:/Users/benson/Desktop/vscode/gym-pybullet-drones-main/gym-pybullet-drones-main/gym_pybullet_drones')
+# from utils.enums import DroneModel, Physics, ImageType
+
+# import pybullet
+
+from transforms3d.quaternions import rotate_vector, qconjugate
